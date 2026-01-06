38816a54c447845b0ac848de907502b38400b806
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2026-01-06 13:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+onst codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener("keydown", (e) => {
+
+    if (e.key >= 0 && e.key <= 9) {
+      code.value = "";
+      setTimeout(() => {
+        if (codes[index + 1]) {
+          codes[index + 1].focus();
+        }
+      }, 10);
+    }
+
+    if (e.key === "Backspace") {
+      if (code.value === "" && codes[index - 1]) {
+        codes[index - 1].focus();
+      }
+    }
+  });
+});
